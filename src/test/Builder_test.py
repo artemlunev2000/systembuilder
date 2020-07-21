6fd05d1dad5e6e7c517b5ea591399804e6fc5e58
Lunev Artem <artem.e.lunev2000@gmail.com>
2020-07-21 12:05:14 +0300

    feat: build class

---
+++ src/test/Builder_test.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+from src.main.Builder import Builder
+from time import sleep
+
+
+class BuilderTest:
+    def test_clone_repo(self):
+        builder = Builder()
+        builder.clone_repo('https://github.com/antonkurenkov/systembuilder.git')
+        # waiting while cloning
+        sleep(5)
+        assert os.path.isfile('./repo/README.md')
+        # deleting directory after testing, might not work on windows because can't delete .git file.
+        #shutil.rmtree('./repo', ignore_errors=True)
