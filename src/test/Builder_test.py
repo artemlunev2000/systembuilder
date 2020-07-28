32f5b0358467dccd130ebfd3f5e8c8720ba7ef5d
Lunev Artem <artem.e.lunev2000@gmail.com>
2020-07-28 18:09:59 +0300

    fix: tests

--- src/test/Builder_test.py
+++ src/test/Builder_test.py
@@ -5,12 +5,11 @@ from src.main.Builder import Builder
 from time import sleep
 
 
-class BuilderTest:
-    def test_clone_repo(self):
-        builder = Builder()
-        builder.clone_repo('https://github.com/antonkurenkov/systembuilder.git')
-        # waiting while cloning
-        sleep(5)
-        assert os.path.isfile('./repo/README.md')
-        # deleting directory after testing, might not work on windows because can't delete .git file.
-        #shutil.rmtree('./repo', ignore_errors=True)
+def test_clone_repo():
+    builder = Builder()
+    builder.clone_repo('https://github.com/antonkurenkov/systembuilder.git')
+    # waiting while cloning
+    sleep(5)
+    assert os.path.isfile('./repo/README.md')
+    # deleting directory after testing, might not work on windows because can't delete .git file.
+    #shutil.rmtree('./repo', ignore_errors=True)
