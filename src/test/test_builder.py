2c2aa6cce898af1446dc79ce170d8720e983e4bb
Efimi5 <45824703+Efimi5@users.noreply.github.com>
2020-08-03 18:30:15 +0300

    ci: fix syntax

--- src/test/test_builder.py
+++ src/test/test_builder.py
@@ -4,12 +4,12 @@ import shutil
 from src.main.builder import Builder
 from time import sleep
 
-
-def test_clone_repo():
-    builder = Builder()
-    builder.clone_repo('https://github.com/antonkurenkov/systembuilder.git')
-    # waiting while cloning
-    sleep(5)
-    assert os.path.isfile('./repo/README.md')
-    # deleting directory after testing, might not work on windows because can't delete .git file.
-    #shutil.rmtree('./repo', ignore_errors=True)
+class TestBuilder:
+	def test_clone_repo():
+	    builder = Builder()
+	    builder.clone_repo('https://github.com/antonkurenkov/systembuilder.git')
+	    # waiting while cloning
+	    sleep(5)
+	    assert os.path.isfile('./repo/README.md')
+	    # deleting directory after testing, might not work on windows because can't delete .git file.
+	    #shutil.rmtree('./repo', ignore_errors=True)
